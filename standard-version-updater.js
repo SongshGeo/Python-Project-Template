46fdbb6b7c3f32e9186b20346b63cfa9b7d65a4b
SongshGeo <songshgeo@gmail.com>
2024-11-18 11:31:02 +0100

    fix: :green_heart: 修正版本更新器

--- standard-version-updater.js
+++ standard-version-updater.js
@@ -1,15 +1,9 @@
-const stringifyPackage = require('stringify-package')
-const detectIndent = require('detect-indent')
-const detectNewline = require('detect-newline')
-
 module.exports.readVersion = function (contents) {
   const match = contents.match(/version\s*=\s*"([^"]+)"/)
   return match ? match[1] : '0.0.0'
 }
 
 module.exports.writeVersion = function (contents, version) {
-  const indent = detectIndent(contents).indent
-  const newline = detectNewline(contents)
   return contents.replace(
     /version\s*=\s*"[^"]+"/,
     `version = "${version}"`
